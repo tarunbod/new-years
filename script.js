bf84e9ac156e8fb13500fceb0dbcdd54fc3d0743
Tarun Boddupalli <tarunbgamer@gmail.com>
2014-12-27 20:05:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-  $("body").on("touchstart", function(e) {
-    e.preventDefault();
-  });
   $("img").on("webkitAnimationEnd", function() {
       $("p, h1").animate({opacity:1});
   });
